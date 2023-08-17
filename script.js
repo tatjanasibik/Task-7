4649fe715ae30bf3a04e369fb7e9e79f78525672
DESKTOP-R5P3GEM\CodeAcademy <tatjana.sibik@gmail.com>
2023-08-17 21:49:09 +0300

    Ikelimas

---
+++ script.js
@@ -0,0 +1,20 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
+
+let showObjectKeys = Object.keys(audi);
+
+
+
+console.log(showObjectKeys);
+
+document.getElementById("res").innerHTML = showObjectKeys;
